a74be3176c6f3c0a2e7e0cf16839113046cf0c6b
MARK <joxa030@gmail.com>
2026-05-26 17:07:16 +0900

    feat: TASK-M

--- train.py
+++ train.py
@@ -1,16 +1,32 @@
+# TASK-M
+
+def getSquareNumbers(numbers):
+    result = []
+    for number in numbers:
+        obj = {
+           "number": number,
+           "square": number ** 2 
+        }
+        result.append(obj)
+       
+    return result
+    
+print(getSquareNumbers([1, 2, 3]))
+
+
 # TASK-l
 
 
-def reverseSentence(text):
-    words = text.split()
-    new_words = []
-    
-    
-    for word in words:
-        reversed_word = word[::-1]
-        new_words.append(reversed_word)
-    return " ".join(new_words) 
+# def reverseSentence(text):
+#     words = text.split()
+#     new_words = []
 
-result = reverseSentence("we like coding!")
-   
-print(result)   
+
+#     for word in words:
+#         reversed_word = word[::-1]
+#         new_words.append(reversed_word)
+#     return " ".join(new_words)
+
+# result = reverseSentence("we like coding!")
+
+# print(result)
