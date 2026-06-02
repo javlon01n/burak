c589d6432f6a4568410a2f5abaa130349c07f2fd
MARK <joxa030@gmail.com>
2026-06-02 15:18:16 +0900

    feat: create project standards

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -5,6 +5,7 @@ import MemberService from '../models/Member.service';
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
+        console.log("goHome")
         res.send("Home Page");
 
     } catch (err) {
---
+++ src/train.ts
@@ -0,0 +1,10 @@
+/*Project Standards:
+- Logging stanadards:
+- Naming standads:
+   function, method, variable => CAMEL
+    class => PASCAL    
+    folder => KEBAB
+    css => SNAKE
+- Error handling
+
+*/
