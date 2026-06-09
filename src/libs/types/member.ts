780aa3cbc092b89494b091577b3c5998a58aee39
MARK <joxa030@gmail.com>
2026-06-09 15:30:01 +0900

    fix: create sessions on login and signup process

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,6 +1,11 @@
+import { ObjectId } from "mongoose";
 import { MemberStatus, MemberType } from "../enums/member.enum";
+import { Request } from "express";
+import { Session } from 'express-session';
+import { ObjectID } from '../../../node_modules/connect-mongodb-session/node_modules/mongodb/src/index';
 
 export interface Member {
+    _id: ObjectId
 memberType: MemberType; 
 memberStatus: MemberStatus; 
 memberNick: string; 
@@ -30,4 +35,10 @@ memberPoints?: number;
 export interface LoginInput {
     memberNick: string; 
     memberPassword: string;
+}
+
+
+export interface AdminRequest extends Request {
+    member: Member;
+    session:Session & { member: Member };
 }
