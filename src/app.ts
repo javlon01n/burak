0da23209f1b0703009c92fbd59649cb1496c58f2
MARK <joxa030@gmail.com>
2026-06-16 14:26:17 +0900

    fix: modify restaurant controller processSignup and processLogin

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,10 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan"
 import { MORGAN_FORMAT } from "./libs/config";
 
+//TSP2
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
 import { T } from "./libs/types/common";
-
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
     uri: String(process.env.MONGO_URL),
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import mongoose from "mongoose";
 import app from "./app"
 
+//TSP 1
 mongoose
 .connect(process.env.MONGO_URL as string, {})
 .then((data) => {
