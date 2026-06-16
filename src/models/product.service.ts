35ae444191b32cdaad443f0ac85902842531df2d
MARK <joxa030@gmail.com>
2026-06-16 16:31:05 +0900

    feat: createNewProduct business logic by restaurnt member

--- src/models/product.service.ts
+++ src/models/product.service.ts
@@ -1,3 +1,5 @@
+import Errors, { HttpCode, Message } from "../libs/Errors";
+import { Product, ProductInput } from "../libs/types/product";
 import productModel from "../schema/product.model";
 
 
@@ -7,6 +9,25 @@ class ProductService {
     constructor() {
            this.productModel = productModel;
         }
+
+/** SPA */
+
+
+
+
+
+
+
+/** SSR */ 
+
+public async createNewProduct(input: ProductInput): Promise<Product> {
+    try { 
+        return await this.productModel.create(input);
+    }   catch (err) {
+            console.error("Error, model:createNewProduct:", err);
+            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        }
+    }
 }
 
 export default ProductService;
