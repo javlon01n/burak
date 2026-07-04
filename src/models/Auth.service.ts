392f1fa652ae663bdf6323ba883205df968be0f1
MARK <joxa030@gmail.com>
2026-07-04 17:14:31 +0900

    feat: develop verify  authenticated user createntials

--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -4,14 +4,18 @@ import { Member } from "../libs/types/member";
 import  jwt  from "jsonwebtoken";
 
 class AuthService {
-    constructor() {}
+    private readonly secretToken;
+
+    constructor() {
+        this.secretToken = process.env.SECRET_TOKEN as string;    
+    }
  
     public async createToken (payload: Member) {
         return new Promise ((resolve, reject) => {
             const duration = `${AUTH_TIMER}h`;
             jwt.sign(
                 payload,
-                process. env.SECRET_TOKEN as string,
+                process.env.SECRET_TOKEN as string,
                 {
                   expiresIn: duration,
                 },
@@ -25,6 +29,15 @@ class AuthService {
             );
         });
     }
+
+public async checkAuth (token: string): Promise<Member> {
+    const result: Member = (await jwt. verify(
+        token, this. secretToken
+    )) as Member;
+    console.log (`---[AUTH] memberNick: ${result.memberNick} —--`);
+    return result;
+    }
+
 }
 
 export default AuthService
