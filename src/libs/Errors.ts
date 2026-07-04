b88775f3ac6d36795600e03fc41924d0aff0ec2c
MARK <joxa030@gmail.com>
2026-07-04 14:54:53 +0900

    feat: develop tokrn creation via jwt

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -16,10 +16,11 @@ export enum Message {
     UPDATE_FAILED = "Update is failed",
 
     USED_NICK_PHONE = "You are inserting already used nick or phone!",
+    TOKEN_CRETION_FILED = "Token creation error!",
     NO_MEMBER_NICK = "No member with that member nick!",
     BLOCKED_USER = "You have been blocked, contact restaurant!",
     WRONG_PASSWORD = "Wrong password, please try again!",
-    NOT_AUTHENTICATED = "You are not authenticated, please login first!"
+    NOT_AUTHENTICATED = "You are not authenticated, please login first!" 
 }
 
 class Errors extends Error {
